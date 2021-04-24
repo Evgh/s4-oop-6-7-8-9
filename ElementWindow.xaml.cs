006f61b47812b97af6c1c8b346405217253d96b5
Evgh <kasperovich.official@gmail.com>
2021-04-24 14:02:31 +0300

    Чуть доработала логику кнопок в диалоговом окне, исправила баг

--- ElementWindow.xaml.cs
+++ ElementWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace s4_oop_6_7_8_9
             InitializeComponent();
         }
 
-        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        private void CloseWindow(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
